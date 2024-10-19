b3e40e5daabfd049f361cee483b21ffdeeb2ffd0
FedeFagotti <fede.jf@hotmail.com>
2024-10-19 12:07:12 -0300

    More print examples

---
+++ laboratorio operaciones aritmeticas.py
@@ -0,0 +1,21 @@
+# Ejercicio 1
+a = 5
+b = 7
+c = 10
+resultado = a + b + c
+print("a:")
+print(a)
+print("b:")
+print(b)
+print("c:")
+print(c)
+print("resultado:")
+print(resultado)
+
+
+# Ejercicio 2
+saludo = "Hola, "
+nombre = "Fede"
+resultado = saludo + nombre
+print(resultado)
+print ("Hola, " + nombre)
