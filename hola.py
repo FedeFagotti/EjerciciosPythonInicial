34525f3b86fb982ac4137b50e03c5e70c8e60d5b
FedeFagotti <fede.jf@hotmail.com>
2024-10-19 12:03:51 -0300

    Print examples

---
+++ hola.py
@@ -0,0 +1,32 @@
+# 1. Comando Print, imprime mensajes en la consola.
+print("hola mundo")
+print("Hoy es Sabado")
+print("Mañana es Domingo")
+
+#Esto es un comentario
+#Para poner varios comentarios en simultaneo es: ctrl + k + c. Y para sacar varios comentarios a la vez: ctrl + k + u.
+
+print (5) #Los numeros no necesitan comillas, es un codigo valido por Python.
+
+# 2. Operaciones aritmeticas.
+
+#Expresion = Es una porción de código que ejecuta una operacion y devuelve un resultado.
+
+
+print("El resultado de 7 + 5 es:", 7 + 5)
+print(7 - 5)
+print(7 * 5)
+print(7 / 5)
+
+#Prioridad de operadores:
+# primero multiplicaciones y divisiones, y luego sumas y restas. La prioridad es igual a las matematicas.
+
+print(7 + 5 - 2)
+print(7 * 5 / 2)
+print(7 + 5 * 2)
+#Puedo alterar la prioridad usando parentesis.
+print((7 + 5) * 2)
+
+#Para elevar algo se hace con dos asteriscos (**)
+
+print(12 ** 2)
