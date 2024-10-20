13250d9ff7d996edf88493526c9af35abc80a8b6
FedeFagotti <fede.jf@hotmail.com>
2024-10-20 19:11:06 -0300

    conditionals examples

---
+++ laboratorio condicionales.py
@@ -0,0 +1,29 @@
+#Ejercicio 1
+
+texto_uno = "potente"
+texto_dos = "sol"
+texto_tres = "triunfo"
+print("El", texto_dos, "en verano esta muy", texto_uno, "eso es un", texto_tres, "para las personas que les gusta el calor.")
+
+
+
+#Ejercicio 2
+
+base = 10
+altura = 5
+area = base * altura
+print("El area del rectangulo es de", area)
+
+
+#Ejercicio 3
+
+a = 20
+b = 10
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+print("La suma es:", suma)
+print("La resta es:",resta)
+print("La multiplicación es:", multiplicacion)
+print("La división es:", division)
