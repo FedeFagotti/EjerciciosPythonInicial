1bf11b9873d8c61d7363fdf8d4caababf5ef1961
FedeFagotti <fede.jf@hotmail.com>
2024-10-21 20:29:45 -0300

    input exercise

---
+++ laboratorio adicional1 entradas.py
@@ -0,0 +1,32 @@
+# Ejercicio 1
+
+a = input("Ingrese una cadena: ")
+b = input("Ingrese otra cadena: ")
+
+if a == b:
+    print("Las cadenas son iguales.")
+
+else:
+    print("las cadenas son diferentes.")
+
+
+#Ejercicio 2
+
+nombre = input("Ingrese un nombre: ")
+
+if nombre == "":
+    print("Error! Ingrese un nombre.")
+
+else:
+    print("Ha ingresado un nombre correctamente.")
+
+
+#Ejercicio 3
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Es mayor de edad.")
+
+else:
+    print("No es mayor de edad.")
