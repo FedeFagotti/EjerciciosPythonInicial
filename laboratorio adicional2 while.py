470dca18a4063b2729619bb9387326fa42b97d57
FedeFagotti <fede.jf@hotmail.com>
2024-10-21 20:31:28 -0300

    while exercise

---
+++ laboratorio adicional2 while.py
@@ -0,0 +1,21 @@
+# Ejercicio 1
+
+a = 0
+
+while a < 10:
+    print(a)
+    a = a + 1
+
+
+# Ejercicio 2
+
+
+nombre = input("Ingrese su usuario: ")
+
+while nombre == "":
+        print("Error de ingreso.")
+        nombre = input("Por favor, ingrese su usuario nuevamente: ")
+        
+print("Bienvenido", nombre)
+
+    
